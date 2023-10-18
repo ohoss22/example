ada41ed7a3402b61a40c08c9866d5fecba6c2926
ohoss22 <97784719+ohoss22@users.noreply.github.com>
2023-10-18 11:35:57 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+        while(1)
+        {
+                fprintf(stderr, "Hello World!");
+                sleep(1);
+        }
+        return 0;
+}
